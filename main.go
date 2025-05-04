bacfddcefb0ecddada222525609344360f3ab134
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-05-04 22:50:45 +0530

    calc main loop

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
+import "fmt"
+
+const VERSION = "1.0.0"
+
 func main() {
-	println("Hello, World!")
+	fmt.Println("calc", VERSION)
+
+	fmt.Println("\n ('q' for quit)")
+	for {
+		fmt.Print(">> ")
+		//read input expression
+		exp := readline()
+
+		if exp == "q" {
+			break
+		} else if exp == "" {
+			continue
+		}
+
+		//debug print
+		println("input:", exp)
+
+	}
+}
+
+func readline() string {
+	var text = ""
+	for {
+		var temp rune
+		fmt.Scanf("%c", &temp)
+		if temp == '\n' {
+			break
+		}
+		text += string(temp)
+	}
+	return text
+
 }
