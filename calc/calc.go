d35043f1dec971b15dfc96a11b410c89e0a87e54
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-05-04 23:21:39 +0530

    check for invalid symbols in input expression

--- calc/calc.go
+++ calc/calc.go
@@ -1,6 +1,42 @@
 package calc
 
+import (
+	"fmt"
+	"strings"
+)
+
 func Calc(exp string) {
+	//remove leading and trailing spaces
+	exp = strings.TrimSpace(exp)
+
+	//check for invalid symbols in expression
+	if !valid_symbols(exp) {
+		fmt.Println("invalid expression!")
+		return
+	}
+
 	//debug print
-	println("result:", exp)
+	fmt.Println(exp)
+}
+
+func valid_symbols(exp string) bool {
+	var valid_symbols = []rune{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '+', '-', '*', '/', '%', '(', ')'}
+
+	for _, e := range exp {
+		valid := false
+		for _, vs := range valid_symbols {
+			if e == vs {
+				valid = true
+				break
+			}
+		}
+
+		if valid {
+			continue
+		} else {
+			return false
+		}
+	}
+
+	return true
 }
